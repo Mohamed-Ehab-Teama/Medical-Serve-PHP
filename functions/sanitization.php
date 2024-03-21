62b8680207f2fc24ddf982c3101c54bfa9a0c398
Mohamed Ehab Mohamed Teama <mohamed111.me111@gmail.com>
2024-03-21 17:26:59 +0200

    sanitize

--- functions/sanitization.php
+++ functions/sanitization.php
@@ -1,6 +1,13 @@
 <?php 
 
+// This is to sanitize the input data 
+/*
+we will apply in in the input of :
+    - Add Admin
+    - Add City
+    - Add Service
 
+*/
 function cleanData($value){
 
 $res = trim( htmlentities( htmlspecialchars($value) ) );
